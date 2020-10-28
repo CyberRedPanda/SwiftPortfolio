615bcdf0eb79c23f6457919f23f236ead2ba8a82
Bryan <Bryan>
2020-10-28 09:36:58 -0400

    Initial Commit

---
+++ SwiftPortfolio/SwiftPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPortfolioApp.swift
+//  SwiftPortfolio
+//
+//  Created by User23198271 on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
